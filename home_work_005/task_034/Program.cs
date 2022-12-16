0644591026160db891013d76f5efbeca1821e55b
albert <albkar@gmail.com>
2022-12-16 20:34:16 +0300

    34,36

---
+++ home_work_005/task_034/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+Console.WriteLine("Введите количество элементов в массиве");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayGen(int Length)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int NumberOfEven(int[] array)
+{
+    int amount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            amount++;
+        }
+
+    }
+    return amount;
+}
+
+void PrintArray(int[] Array)
+{
+    Console.WriteLine("Вывод созданного массива");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]}, ");
+    }
+Console.WriteLine();
+}
+
+int[] array = arrayGen(arrayLength);
+PrintArray(array);
+int amount = NumberOfEven(array);
+
+
+Console.WriteLine($"Количество чётных элементов в массиве равно {amount}");
---
+++ home_work_005/task_036/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+Console.WriteLine("Введите количество элементов в массиве");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение в массиве");
+int arrayMaxValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение в массиве");
+int arrayMinValue = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayGen(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] Array)
+{
+    Console.WriteLine("Вывод созданного массива");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]}, ");
+    }
+Console.WriteLine();
+}
+
+int SumOfOddPositions(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] array = arrayGen(arrayLength, arrayMinValue, arrayMaxValue);
+PrintArray(array);
+int sum = SumOfOddPositions(array);
+Console.WriteLine($"Сумма элементов массива с нечётными индексами равна {sum}");
