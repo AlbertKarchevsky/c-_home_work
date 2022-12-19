405bcc31de6f52a8274e4e9dcba0b088da0fa933
albert <albkar@gmail.com>
2022-12-19 15:42:37 +0300

    41,43

---
+++ home_work_006/task_041/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+int[] EnteringNumbers(int arrayLength)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        Console.WriteLine($"Введите поочерёдно нажимая Enter числа");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int PositiveNumbers(int[] Array)
+{
+    int positiveNumbers = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+        {
+            positiveNumbers = positiveNumbers + 1;
+        }
+    }
+    return positiveNumbers;
+}
+
+
+Console.WriteLine("Сколько чисел будет введено?");
+int length = Convert.ToInt32(Console.ReadLine());
+
+
+int [] array = EnteringNumbers(length);
+int positiveNumbers = PositiveNumbers(array);
+Console.WriteLine ($"Количество положительных чисел: {positiveNumbers}");
---
+++ home_work_006/task_043/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double IntersectionPointX(int k1, int b1, int k2, int b2)
+{
+    double x = (double)(b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double IntersectionPointY(double x, int k1, int b1)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+
+Console.WriteLine("Введите значение k1 первой прямой");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b1 первой прямой");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2 второй прямой");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2 второй прямой");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+    return;
+}
+
+double x = IntersectionPointX(k1, b1, k2, b2);
+double y = IntersectionPointY(x, k1, b1);
+Console.WriteLine($"Прямые пересекаются в точке ({x}; {y})");
