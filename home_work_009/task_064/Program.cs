4af3702d2c0e54b136cc3499d58c5670111a36b2
albert <albkar@gmail.com>
2022-12-23 19:19:15 +0300

    64,66

---
+++ home_work_009/task_064/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+void PrintRecursion(int N)
+{
+    if (N >= 1)
+    {
+
+        Console.Write(N + ", ");
+        PrintRecursion(N - 1);
+
+    }
+    
+    return;
+}
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вывод натуральных чисел от {N} до 1");
+PrintRecursion(N);
---
+++ home_work_009/task_066/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 66. Задайте значения M и N. Напишите программу, которая найдёт
+ сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int NaturalSum(int M, int N)
+{
+    int sum = 0;
+    for (int i = 0; (N - i) >= M; i++)
+    {
+        sum = sum + (N - i);
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = NaturalSum(M, N);
+Console.WriteLine($"Сумма натуральных элементов между M = {M} и N = {N} равна {sum}");
